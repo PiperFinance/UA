6b0d93d51d340296acc7072c8cccd4451e58ecb7
nfel <nfilsaraee@gmail.com>
2023-06-02 12:08:48 +0330

    Preload Addreses

--- src/views/users.go
+++ src/views/users.go
@@ -23,7 +23,7 @@ func OnlineUsers(c *fiber.Ctx) error {
 
 func AllUsers(c *fiber.Ctx) error {
 	var sessions []*models.Session
-	if res := conf.DB.Model(&models.Session{}).Preload("User").Find(&sessions); res.Error != nil {
+	if res := conf.DB.Model(&models.Session{}).Preload("User").Preload("Addresses").Preload("Device").Find(&sessions); res.Error != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"status": "fail", "error": res.Error.Error(),
 		})
@@ -36,7 +36,7 @@ func AllUsers(c *fiber.Ctx) error {
 
 func OfflineUsers(c *fiber.Ctx) error {
 	var sessions []*models.Session
-	if res := conf.DB.Model(&models.Session{}).Preload("User").Find(&sessions, "expires_at < ?", time.Now().Unix()); res.Error != nil {
+	if res := conf.DB.Model(&models.Session{}).Preload("User").Preload("Addresses").Preload("Device").Find(&sessions, "expires_at < ?", time.Now().Unix()); res.Error != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"status": "fail", "error": res.Error.Error(),
 		})
