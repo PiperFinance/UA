77c833d7a0ad67da53ec722cf9d2bc9fda731292
nfel <nfilsaraee@gmail.com>
2023-06-21 22:32:50 +0330

    ~

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 cd /api
-./pgweb --user=$DB_USER --pass=$DB_PASSWORD --host=$DB_HOST --port=$DB_PORT --db=$DB_NAME --bind=0.0.0.0 -d --listen=$PGWEB_PORT &
+./pgweb --user=$DB_USER --pass=$DB_PASSWORD --host=$DB_HOST --port=$DB_PORT --db=$DB_NAME --bind=0.0.0.0 -d --listen=$PGWEB_PORT --ssl=disable &
 
 ./app
